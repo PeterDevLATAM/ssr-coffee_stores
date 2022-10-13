bca8ca0d8f4b3d6332cf0ece89cc65a78df9f0e1
Peter <101288216+PeterDevLATAM@users.noreply.github.com>
2022-10-13 15:47:02 -0500

    fixing typos

--- components/banner.jsx
+++ components/banner.jsx
@@ -8,7 +8,7 @@ export default function Banner({ buttonText, handleOnClick }) {
         <span className={styles.title1}>Coffee</span>{" "}
         <span className={styles.title2}>Connoisseur</span>
       </h1>
-      <p className={styles.subTitle}>Discover your local coffe shops!</p>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
       <div className={styles.buttonWrapper}>
         <button className={styles.button} onClick={handleOnClick}>
           {buttonText}
